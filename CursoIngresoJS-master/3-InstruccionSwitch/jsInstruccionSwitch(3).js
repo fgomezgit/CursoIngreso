4186df4e1c10563bb1ac943d1bfef1b5465bfa89
fgomezgit <52860395+fgomezgit@users.noreply.github.com>
2019-07-16 17:40:58 -0300

    Add files via upload

--- CursoIngresoJS-master/3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ CursoIngresoJS-master/3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -4,8 +4,15 @@ function mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
-	
+    switch(mesDelAño) 
+    {
+        default:
+            alert("Este mes tiene 30 días o más.");
+            break;
 
-
+        case "Febrero":
+            alert("Este mes no tiene más de 29 días.");
+            break;
+    }
+        
 }//FIN DE LA FUNCIÓN
--- CursoIngresoJS-master/3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ CursoIngresoJS-master/3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -2,11 +2,68 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+switch(mesDelAño)
+    {
+        case "Enero":
+        {
+            alert("Tiene 31 días.");
+            break;
+        }
+        case "Febrero":
+        {
+            alert("Tiene 28 días.");
+            break;
+        }
+        case "Marzo":
+        {
+            alert("Tiene 30 días.");
+            break;
+        }
+        case "Abril":
+        {
+            alert("Tiene 30 días.");
+            break;
+        }
+        case "Mayo":
+        {
+            alert("Tiene 31 días.");
+            break;
+        }
+        case "Junio":
+        {
+            alert("Tiene 30 días.");
+            break;
+        }
+        case "Julio":
+        {
+            alert("Tiene 31 días.");
+            break;
+        }
+        case "Agosto":
+        {
+            alert("Tiene 31 días.");
+            break;
+            }
+        case "Septiembre":
+        {
+            alert("Tiene 30 días.");
+            break;
+        }
+        case "Octubre":
+        {
+            alert("Tiene 31 días.");
+            break;
+        }
+        case "Noviembre":
+        {
+            alert("Tiene 30 días.");
+            break;
+        }
+        case "Diciembre":
+        {
+            alert("Tiene 31 días.");
+            break;
+        }
+    }
 
-//alert (mesDelAño);
-	
-	
-
-
-
-}//FIN DE LA FUNCIÓN
+}
