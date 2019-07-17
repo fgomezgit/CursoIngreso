7dcc1f6879f26611fd3dd986cd0552fe00c44e90
fgomezgit <52860395+fgomezgit@users.noreply.github.com>
2019-07-16 21:34:47 -0300

    resuelto

--- CursoIngresoJS-master/9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ CursoIngresoJS-master/9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,30 @@
 function mostrar()
 {
+    // 1) Declarar variables
+    var planeta;
+    var mensaje;
 
+    // 2) Tomar datos / Inicializar
+    planeta = prompt("Ingrese un planeta.");
+
+    // 3) Procesar datos
+    switch (planeta) {
+        case "mercurio":
+        case "venus":
+            mensaje = "Acá hace mas calor.";
+            break;
+        case "tierra":
+            mensaje = "Acá vivimos"
+            break;
+        case "júpiter":
+        case "saturno":
+        case "urano":
+        case "neptuno":
+        case "plutón":
+            mensaje = "Acá hace mas frío.";
+            break;
+        default:
+            mensaje = "No es un planeta válido.";
+        }
+    alert(mensaje);
 }
